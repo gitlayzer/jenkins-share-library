6dc84392f69b5d2fe9d78d10a82b0987be41ae30
Gitlayzer <2054076300@qq.com>
2022-11-30 01:59:17 +0800

    Update tool.groovy

--- src/org/library/tool.groovy
+++ src/org/library/tool.groovy
@@ -1,10 +1,8 @@
 package org.library
 
-def PrintMes(value, color) {
-    colors = [
-        'red': "033[40;31m >>>>>>>>>>>${value}cc<ccc<<<ee \033[0m",
-        'green' : "[1;32m>>>>>>>>>>${value}>>>>>>>>>>[m",
-    ]
+def PrintMsg(value,color){
+    colors = ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
+              'green' : "[1;32m>>>>>>>>>>${value}>>>>>>>>>>[m",]
     ansiColor('xterm') {
         println(colors[color])
     }
